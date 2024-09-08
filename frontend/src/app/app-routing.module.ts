ea82c669e3ebeb82e37af4336b13b9e9f8a0f659
medAli-ai <bouchhioua.medali@gmail.com>
2024-09-08 17:12:53 +0100

    feat: Add dashboard route to admin layout
    This commit adds the `dashboard` route to the `admin` layout in the `app-routing.module.ts` file. It includes the necessary changes to the routing configuration, allowing the `DashboardComponent` to be displayed within the `AdminLayoutComponent`. This feature enhances the application by providing a separate route for the dashboard functionality, improving the overall organization and user experience.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './views/admin/dashboard/dashboard/dashboard.component';
+
 
 const routes: Routes = [
   {path:'', component:FrontLayoutComponent},
-  {path:'admin', component:AdminLayoutComponent}
+  {path:'admin', component:AdminLayoutComponent,
+    children:[{path:'dashboard', component:DashboardComponent},]
+  }
 ];
 
 @NgModule({
