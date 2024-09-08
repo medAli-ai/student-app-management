d04cce9b6694360cdf3117e69916b3b9fa015a87
medAli-ai <bouchhioua.medali@gmail.com>
2024-09-08 18:21:21 +0100

    feat: Add home and loginuser routes to front layout
    This commit adds the `home` and `loginuser` routes to the `FrontLayoutComponent` in the `app-routing.module.ts` file. It includes the necessary changes to the routing configuration, allowing the `HomeComponent` and `LoginuserComponent` to be displayed within the `FrontLayoutComponent`. This feature enhances the application by providing separate routes for the home and loginuser functionality, improving the overall organization and user experience.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { FrontLayoutComponent } from './layouts/front-layout/front-layout.compon
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { DashboardComponent } from './views/admin/dashboard/dashboard/dashboard.component';
 import { LoginadminComponent } from './views/admin/loginadmin/loginadmin/loginadmin.component';
-
+import { HomeComponent } from './views/front/home/home/home.component';
+import { LoginuserComponent } from './views/front/loginuser/loginuser/loginuser.component';
 
 
 const routes: Routes = [
-  {path:'', component:FrontLayoutComponent},
+  {path:'', component:FrontLayoutComponent, children:[
+    {path:'', component:HomeComponent},
+    {path:'loginuser', component:LoginuserComponent}
+  ]},
   {path:'admin', component:AdminLayoutComponent,
     children:[
       {path:'dashboard', component:DashboardComponent},
