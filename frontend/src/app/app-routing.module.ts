a2f38e80b4d0eb980468dae9eb62f165357f6927
medAli-ai <bouchhioua.medali@gmail.com>
2024-09-10 14:46:58 +0100

    feat: Add routing for front and admin layouts
    The code changes in the `app-routing.module.ts` file add routing configurations for the front and admin layouts. This enhancement improves the navigation of the application by defining routes for the front layout and the admin layout, including a dedicated dashboard route for administrators.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: FrontLayoutComponent},
+  {path:'admin', component: AdminLayoutComponent, 
+    children:[
+      {path:'dashboard', loadChildren: ()=>import('./views/admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
